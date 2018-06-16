cb88bde1cdf511c79876a97dea43554ad0f524df
Taylor Arnold <taylor.arnold@acm.org>
2018-06-16 15:52:58 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer>
-        <p class="view"><a href="http://github.com/statsmaths/dtv-toolkit" target="_blank">View the Distant Viewing Toolkit (DTV) on GitHub <small>statsmaths/dtv</small></a></p>
+        <p class="view"><a href="http://github.com/statsmaths/dtv" target="_blank">View the Distant Viewing Toolkit (DTV) on GitHub <small>statsmaths/dtv</small></a></p>
         <p>By <a href="http://github.com/statsmaths" target="_blank">Taylor Arnold</a> and <a href="http://laurentilton.com" target="_blank">Lauren Tilton</a></p>
       </footer>
     </div>
